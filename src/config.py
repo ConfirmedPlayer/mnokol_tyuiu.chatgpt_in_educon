d65877e627e19183e610acd900d71b7dbba4dbf0
ConfirmedPlayer <confirmedplayer@mail.ru>
2023-09-08 22:53:03 +0500

    dfsfsdsdf

--- src/config.py
+++ src/config.py
@@ -6,11 +6,11 @@ EDUCON_API_URL = 'https://educon2.tyuiu.ru/lib/ajax/service.php'
 
 EDUCON_SESSION_PROFILE_ID = 47759
 
-EDUCON_LOGIN = os.environ.get('EDUCON_LOGIN', 'lagutkinvd@std.tyuiu.ru')
-EDUCON_PASSWORD = os.environ.get('EDUCON_PASSWORD', 'Temnomor.ru!0')
+EDUCON_LOGIN = os.environ.get('EDUCON_LOGIN')
+EDUCON_PASSWORD = os.environ.get('EDUCON_PASSWORD')
 
 
-CHATGPT_TOKEN = os.environ.get('CHATGPT_API_TOKEN', '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')
+CHATGPT_TOKEN = os.environ.get('CHATGPT_API_TOKEN')
 
 
 TIMEZONE = 'Asia/Yekaterinburg'
