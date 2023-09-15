c131e59321ae35ae45589b191b4057ad3368224e
ConfirmedPlayer <confirmedplayer@mail.ru>
2023-09-15 13:06:43 +0500

    adsadsa

--- src/chatgpt.py
+++ src/chatgpt.py
@@ -3,6 +3,14 @@ import openai
 from config import CHATGPT_SYSTEM_PROMPT, CHATGPT_TOKEN
 
 
+system_prompt = '''
+Пользователь отправляет тебе вопрос из тестирования, и варианты ответа.
+Ты должен внимательно изучить вопрос, и выбрать ответ из тех, который тебе дал пользователь.
+Ты должен просто отправить ответ, точно в таком же виде, какой он был написан в варианте ответов.
+Если вариантов ответа нет, ты должен просто предоставить ответ.
+'''
+
+
 class ChatGPT:
     def __init__(self, system_prompt: str = CHATGPT_SYSTEM_PROMPT) -> None:
         self.system_prompt = system_prompt
@@ -10,6 +18,18 @@ class ChatGPT:
 
     async def ask(self, message: str):
         try:
+            if message.startswith('/решение_теста'):
+                edited_message = message.replace('/решение_теста ', '', 1)
+                request = await openai.ChatCompletion.acreate(
+                    model='gpt-3.5-turbo',
+                    messages=[
+                        {'role': 'system', 'content': system_prompt},
+                        {'role': 'user', 'content': edited_message}
+                    ]
+                )
+                response = request.choices[0].message.content
+                return response
+
             request = await openai.ChatCompletion.acreate(
                 model='gpt-3.5-turbo',
                 messages=[
